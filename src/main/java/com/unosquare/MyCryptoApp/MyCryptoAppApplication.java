17847323c2b66fabaf12e0aa177d626d9144a360
Jaxon <jaxon@somosmakers.co>
2022-03-23 23:36:03 -0500

    Base template

---
+++ src/main/java/com/unosquare/MyCryptoApp/MyCryptoAppApplication.java
@@ -0,0 +1,13 @@
+package com.unosquare.MyCryptoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCryptoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCryptoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unosquare/MyCryptoApp/MyCryptoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unosquare.MyCryptoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCryptoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
